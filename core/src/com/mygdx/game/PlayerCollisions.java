b375c5ee9506b06ff981f17a0f17b0327bb4b47d
superjw2 <superjoshw@gmail.com>
2022-01-29 14:02:37 -0600

    fixed null pointer in collision checks

--- core/src/com/mygdx/game/PlayerCollisions.java
+++ core/src/com/mygdx/game/PlayerCollisions.java
@@ -30,62 +30,61 @@ public class PlayerCollisions {
                 Fixture other = null;
                 Fixture falling = null;
 
-                if (fixA.getBody().getUserData().toString() == "falling") {
-                    falling = fixA;
-                } else if (fixB.getBody().getUserData().toString() == "falling") {
-                    falling = fixB;
-                }
-
-                if (fixA.getBody().getUserData().toString() == "Travis") {
-                    travis = fixA;
-                    other = fixB;
-                } else if (fixB.getBody().getUserData().toString() == "Travis") {
-                    travis = fixB;
-                    other = fixA;
-                }
-
-                if (falling != null) {
-                    
-                }
-
-                if (travis != null && travis.getBody().getUserData() instanceof Player) {
-                    Player trueTravis = (Player) travis.getBody().getUserData();
-                    switch (other.getBody().getUserData().toString()) {
-                        case "sticky" :
-                            trueTravis.sticky = true;
-                            System.out.println("Such stick,  Wow");
-                        break;
-                        case "bouncy" :
-                            //trueTravis.bounce();
-                            System.out.println("Boing!");
-                        break;
-                        case "sliderleft" :
-                            //trueTravis.sliderleft;
-                            System.out.println("<-----");
-                        break;
-                        case "sliderright" :
-                            //trueTravis.sliderright;
-                            System.out.println("----->");
-                        break;
-                        case "gravityup" :
-                            trueTravis.gravityDirection = "up";
-                            System.out.println("Up We Go");
-                        break;
-                        case "spike" :
-                            //call reset method?
-                            System.out.println("Ouch!");
-                        break;
-
+                if (fixA.getBody().getUserData() != null && fixB.getBody().getUserData() != null) {
+                    if (fixA.getBody().getUserData().toString() == "falling") {
+                        falling = fixA;
+                        other = fixB;
+                    } else if (fixB.getBody().getUserData().toString() == "falling") {
+                        falling = fixB;
+                        other = fixA;
                     }
-                }
+                    /*
+                    if (falling != null && falling.getBody().getUserData() instanceof FallingTile) {
+                        if (falling.untouched && other.getBody().getUserData().toString() == "Travis") {
+                            falling.fall();
+                        } else if (!falling.untouched && (other.getBody().getUserData() instanceof Tile) && (!other.getBody().getUserData() instanceof Spike)) {
+                            falling.poof();
+                        }
+                    }
+                    */
+                    if (fixA.getBody().getUserData().toString() == "Travis") {
+                        travis = fixA;
+                        other = fixB;
+                    } else if (fixB.getBody().getUserData().toString() == "Travis") {
+                        travis = fixB;
+                        other = fixA;
+                    }
 
-                //do collide here
-                if (fixA.getBody().getUserData() == "Travis") {
-                    System.out.println(fixA.getBody().getUserData() + " has collided with " +  fixB.getBody().getType());
-                } else if (fixB.getBody().getUserData() == "Travis") {
-                    System.out.println(fixB.getBody().getUserData() + " has collided with " +  fixA.getBody().getType());
-                } else {
-                System.out.println(fixA.getBody().getType() + " has collided with " + fixB.getBody().getType());
+                    if (travis != null && travis.getBody().getUserData() instanceof Player) {
+                        Player trueTravis = (Player) travis.getBody().getUserData();
+                        switch (other.getBody().getUserData().toString()) {
+                            case "sticky" :
+                                trueTravis.sticky = true;
+                                System.out.println("Such stick,  Wow");
+                            break;
+                            case "bouncy" :
+                                //trueTravis.bounce();
+                                System.out.println("Boing!");
+                            break;
+                            case "sliderleft" :
+                                //trueTravis.sliderleft;
+                                System.out.println("<-----");
+                            break;
+                            case "sliderright" :
+                                //trueTravis.sliderright;
+                                System.out.println("----->");
+                            break;
+                            case "gravityup" :
+                                trueTravis.gravityDirection = "up";
+                                System.out.println("Up We Go");
+                            break;
+                            case "spike" :
+                                //call reset method?
+                                System.out.println("Ouch!");
+                            break;
+
+                        }
+                    }
                 }
             }
 
@@ -93,24 +92,51 @@ public class PlayerCollisions {
             public void endContact(Contact contact) {
                 Fixture fixA = contact.getFixtureA();
                 Fixture fixB = contact.getFixtureB();
-                Fixture travis;
-                Fixture other;
+                Fixture travis = null;
+                Fixture other = null;
 
-                if (fixA.getBody().getUserData() == "Travis") {
-                    travis = fixA;
-                    other = fixB;
-                } else if (fixB.getBody().getUserData() == "Travis") {
-                    travis = fixB;
-                    other = fixA;
-                }
 
-                //do no more collide
-                if (fixA.getBody().getUserData() == "Travis") {
-                    System.out.println(fixA.getBody().getUserData() + " has stopped collide with " +  fixB.getBody().getType());
-                } else if (fixB.getBody().getUserData() == "Travis") {
-                    System.out.println(fixB.getBody().getUserData() + " has stopped collide with " +  fixA.getBody().getType());
-                } else {
-                System.out.println(fixA.getBody().getType() + " has stopped collide with " + fixB.getBody().getType());
+                if (fixA.getBody().getUserData() != null && fixB.getBody().getUserData() != null) {
+                    if (fixA.getBody().getUserData() == "Travis") {
+                        travis = fixA;
+                        other = fixB;
+                    } else if (fixB.getBody().getUserData() == "Travis") {
+                        travis = fixB;
+                        other = fixA;
+                    }
+
+                    if (travis != null && travis.getBody().getUserData() instanceof Player) {
+                        Player trueTravis = (Player) travis.getBody().getUserData();
+                        switch (other.getBody().getUserData().toString()) {
+                            case "sticky" :
+                                trueTravis.sticky = false;
+                                System.out.println("No more stick");
+                            break;
+                            //bouncy only needs to get called once, I suspect, and doesn't need an end condition
+                            //case "bouncy" :
+                                //trueTravis.bounce();
+                            //    System.out.println("Boing!");
+                            //break;
+                            case "sliderleft" :
+                                //trueTravis.sliderleft;
+                                System.out.println("<--x--");
+                            break;
+                            case "sliderright" :
+                                //trueTravis.sliderright;
+                                System.out.println("--x-->");
+                            break;
+                            case "gravityup" :
+                                trueTravis.gravityDirection = "down";
+                                System.out.println("Back down");
+                            break;
+                            //since the case resets, and probably calls a method, it shouldnt need an end case
+                            //case "spike" :
+                                //call reset method?
+                            //    System.out.println("Ouch!");
+                            //break;
+
+                        }
+                    }
                 }
             }
 
@@ -125,7 +151,6 @@ public class PlayerCollisions {
             }
         };
         return listener;
-
-    }
+        }
 
 }
