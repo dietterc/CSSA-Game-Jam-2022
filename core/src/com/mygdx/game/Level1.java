e2151e1e575f34219497de93628ba6fb6567bed7
superjw2 <superjoshw@gmail.com>
2022-01-28 23:16:08 -0600

    Player movement and variable gravity

--- core/src/com/mygdx/game/Level1.java
+++ core/src/com/mygdx/game/Level1.java
@@ -23,7 +23,8 @@ public class Level1 implements Screen {
 		this.game = game;
 
 		camera = new OrthographicCamera(WIDTH,HEIGHT);
-        world = new World(new Vector2(0,-10), true);
+        //world = new World(new Vector2(0,-10), true);
+        world = new World(new Vector2(0,0), true);
         debugRenderer = new Box2DDebugRenderer();
 
         player = new Player(world,0,0,camera);
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -22,6 +23,20 @@ public class Player {
     private Sprite bodySprite;
 
     private OrthographicCamera camera;
+
+
+    private Float baseMoveSpeed = 7f;
+    private Float baseJumpStrength = 2.1f;
+    private Float baseJumpDecay = 0.25f;
+
+    private Float gravityStrength = -10f;
+    private String gravityDirection = "down";
+
+    private Boolean landed = false;
+    private Float jumping = 0f;
+
+    private Vector2 speed = new Vector2(0f,0f);
+    private Vector2 pos = new Vector2(0f,0f);
 
     public Player(World world,int startX, int startY, OrthographicCamera c) {
         
@@ -49,13 +64,101 @@ public class Player {
 
     }
 
+
+
+    private void applyForce(float x, float y) {
+        physicsBody.applyForceToCenter(x,y,true);
+    }
+
+    private void checkControls() {
+        //left
+        if (Gdx.input.isKeyPressed(Keys.S) && !Gdx.input.isKeyPressed(Keys.A)) {
+            applyForce(baseMoveSpeed,0f);
+        } 
+        //right
+        if (Gdx.input.isKeyPressed(Keys.A) && !Gdx.input.isKeyPressed(Keys.S)) {
+            applyForce(-baseMoveSpeed,0f);
+        }
+        //jump
+        if (Gdx.input.isKeyPressed(Keys.SPACE) && landed && jumping == 0) {
+            jumping = baseJumpStrength;
+            //physicsBody.applyLinearImpulse(0f,jumping,pos.x,pos.y,true);
+        }
+
+    }
+
+    private void manageJump() {
+        //continue jump height
+        if (jumping > 0f) {
+            if (Gdx.input.isKeyPressed(Keys.SPACE)) {
+                physicsBody.applyLinearImpulse(0f,jumping,pos.x,pos.y,true);
+                jumping -= baseJumpDecay;
+            } 
+            //stop jumping and start falling
+            if (!Gdx.input.isKeyPressed(Keys.SPACE)) {
+                jumping = 0f;
+            }
+        } else if (jumping != 0f) {
+            jumping = 0f;
+        }
+    }   
+
+    private void updateVars() {
+        speed = physicsBody.getLinearVelocity();
+        if (speed.y == 0f) {
+            landed = true;
+            jumping = 0f;
+        } else {
+            landed = false;
+        }
+        pos = physicsBody.getPosition();
+    }
+
+    private void applyGravity() {
+        switch (gravityDirection) {
+            case "down" :
+                physicsBody.applyForceToCenter(new Vector2(0,gravityStrength),true);
+                break;
+            case "up" :
+                physicsBody.applyForceToCenter(new Vector2(0,-gravityStrength),true);
+                break;
+            case "left" :
+                physicsBody.applyForceToCenter(new Vector2(gravityStrength,0),true);
+                break;
+            case "right" :
+                physicsBody.applyForceToCenter(new Vector2(-gravityStrength,0),true);
+                break;
+        }
+    }
+
+    private void debugGravityChange() {
+        if (Gdx.input.isKeyPressed(Keys.UP)) {
+            gravityDirection = "up";
+        }
+        if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+            gravityDirection = "down";
+        }
+        if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+            gravityDirection = "left";
+        }
+        if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            gravityDirection = "right";
+        }
+    }
+
     //called every iteration of render
     public void step() {
+        checkControls();
+        manageJump();
+        updateVars();
+        applyGravity();
         //escape to exit game
         if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
             Gdx.app.exit();
         }
-        
+        //DEBUGGING!! Remove these later ----------
+        debugGravityChange();
+        //-----------------------------------------
     }
     
     //called every iteration of render
