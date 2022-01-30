ac9e12b4442c8c72583719e78c3bae2bfaa0378f
SiDroid <supersiah7@gmail.com>
2022-01-30 15:25:38 -0600

    rotation

--- core/assets/levels/tileset.tsx
+++ core/assets/levels/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="ASEC P" tilewidth="64" tileheight="64" tilecount="13" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="ASEC P" tilewidth="64" tileheight="64" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="no_jump_block.png"/>
@@ -39,5 +39,8 @@
  </tile>
  <tile id="15">
   <image width="64" height="64" source="invisible_spike.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="spike_down.png"/>
  </tile>
 </tileset>
